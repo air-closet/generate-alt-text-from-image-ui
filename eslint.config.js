a290ee29f93d4d950694a0dcc6d8cd2b601b3c44
masseater <t.masuda@air-closet.com>
2025-05-02 17:13:29 +0900

    chore: lefthookでnpm scriptを使用して、コミット時にlintとformatチェックが実行されるように対応

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,33 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginJsxa11y from "eslint-plugin-jsx-a11y";
-import pluginReact from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import js from '@eslint/js'
+import globals from 'globals'
+import pluginJsxa11y from 'eslint-plugin-jsx-a11y'
+import pluginReact from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules", ".cursor/"] },
+  { ignores: ['dist', 'node_modules', '.cursor/'] },
   {
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
     linterOptions: {
-      reportUnusedDisableDirectives: "error",
+      reportUnusedDisableDirectives: 'error',
     },
   },
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     extends: [js.configs.recommended, eslintConfigPrettier],
   },
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
@@ -37,24 +37,24 @@ export default tseslint.config(
     ],
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     plugins: {
-      "@typescript-eslint": tseslint.plugin,
+      '@typescript-eslint': tseslint.plugin,
       react: pluginReact,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "jsx-a11y": pluginJsxa11y,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': pluginJsxa11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
     },
   }
-);
+)
